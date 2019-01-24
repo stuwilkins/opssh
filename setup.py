20326ab6109fdc18ff644ac73ec56aee913562bf
Stuart B. Wilkins <swilkins@bnl.gov>
2019-01-24 08:11:58 -0500

    Added base files

---
+++ setup.py
@@ -0,0 +1,11 @@
+!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='opssh',
+      version='0.1',
+      description='Python library for interfacing with 1password and ssh keys',
+      author='Stuart B. Wilkins',
+      author_email='stuart@stuwilkins.org',
+      packages=['opssh']
+     )
